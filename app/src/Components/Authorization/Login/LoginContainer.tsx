3a5e83584508e9775a97a2281681f62ca1b8fdcc
Vitaly Knyazev <13.kn.v.v@gmail.com>
2022-05-10 14:11:03 +0300

    refactor

--- app/src/Components/Authorization/Login/LoginContainer.tsx
+++ app/src/Components/Authorization/Login/LoginContainer.tsx
@@ -16,17 +16,16 @@ export const LoginContainer = () => {
   const [onLogin] = useMutation(LOGIN)
   const [errorMessage, setErrorMessage] = useState('')
 
-  const onSubmitLogin = useCallback((dataLoginForm: DataLoginFormType) => {
-    onLogin({ variables: { email: dataLoginForm.email, password: dataLoginForm.password } })
-      .then( response=> {
-        setErrorMessage('')
-        setToken(response.data.login.token)
-        authStore.setCurrentUser(response.data.login.user)
-        authStore.setIsAuth(true)
-      })
-      .catch((error)=> {
+  const onSubmitLogin = useCallback( async (dataLoginForm: DataLoginFormType) => {
+    try {
+      const response = await onLogin({ variables: dataLoginForm })
+      setErrorMessage('')
+      setToken(response.data.login.token)
+      authStore.setCurrentUser(response.data.login.user)
+      authStore.setIsAuth(true)
+    } catch(error: any) { 
         setErrorMessage(error.message)
-      })
+    }
   }, [])
 
   return (
--- app/src/Components/Profile/ProfileContainer.tsx
+++ app/src/Components/Profile/ProfileContainer.tsx
@@ -1,12 +1,10 @@
 import React, { useCallback, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { EDIT_USER } from '../../api/EditUser'
 import { Profile } from './Profile'
 import { useMutation } from '@apollo/client'
 import { useStores } from '../../MobX/stores'
 
 export type NewDataUserType = {
-  id: string
   firstName: string
   secondName: string
   email: string
@@ -19,22 +17,16 @@ export const ProfileContainer = () => {
   const [errorMessage, setErrorMessage] = useState('')
   const [isSaved, setIsSaved] = useState(false)
 
-  const onSubmitEditUser = useCallback((newDataUser:  NewDataUserType) => {
-    onEditUser({ variables: { id: authStore.user.id,
-                              firstName: newDataUser.firstName,
-                              secondName: newDataUser.secondName,
-                              email: newDataUser.email, 
-                              password: newDataUser.password } 
-    })
-      .then( response => {
-        setErrorMessage('')
-        authStore.setCurrentUser(response.data.editUser)
-        setIsSaved(true)
-        setTimeout(() => setIsSaved(false), 3000)
-      })
-      .catch((error)=> {
+  const onSubmitEditUser = useCallback( async (newDataUser:  NewDataUserType) => {
+    try {
+    const response = await onEditUser({ variables: { id: authStore.user.id, ...newDataUser} })
+    setErrorMessage('')
+    authStore.setCurrentUser(response.data.editUser)
+    setIsSaved(true)
+    setTimeout(() => setIsSaved(false), 3000)
+    } catch(error: any) {
         setErrorMessage(error.message)
-      })
+    }
   }, [])
  
 
